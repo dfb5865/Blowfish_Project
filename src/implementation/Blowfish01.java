137c5b826ac3ba48001d942f44f55432644367a8
Stavros Polymenakos <smp7854@rit.edu>
2013-03-24 00:23:43 -0400

    Made a bunch of changes:
    Blowfish01.java
    -Created Standard P,S Boxes so you can change the key after it's been generated once.
    -Implemented keySize()
    -Fixed a mistaken assumption in the initialization of the P-Array
    -Simplified the joining of xL,xR at the end of encrypt and corrected a bug the original code was causing
    
    BlowfishKnownVectorTest.java
    -This test uses all the known key,plaintext,cyphertext sets from http://www.schneier.com/code/vectors.txt and checks that they match.

--- src/implementation/Blowfish01.java
+++ src/implementation/Blowfish01.java
@@ -10,12 +10,12 @@ import edu.rit.util.Packing;
 public class Blowfish01
 		implements BlockCipher{
 
-	int[] P = {
+	int[] StandardP = {
 			0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344, 0xa4093822, 0x299f31d0,
 			0x082efa98, 0xec4e6c89, 0x452821e6, 0x38d01377, 0xbe5466cf, 0x34e90c6c,
 			0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917, 0x9216d5d9, 0x8979fb1b};
 	
-	int[] S0 = {
+	int[] StandardS0 = {
 			0xd1310ba6, 0x98dfb5ac, 0x2ffd72db, 0xd01adfb7, 0xb8e1afed, 0x6a267e96,
 			0xba7c9045, 0xf12c7f99, 0x24a19947, 0xb3916cf7, 0x0801f2e2, 0x858efc16,
 			0x636920d8, 0x71574e69, 0xa458fea3, 0xf4933d7e, 0x0d95748f, 0x728eb658,
@@ -60,7 +60,7 @@ public class Blowfish01
 			0xf296ec6b, 0x2a0dd915, 0xb6636521, 0xe7b9f9b6, 0xff34052e, 0xc5855664,
 			0x53b02d5d, 0xa99f8fa1, 0x08ba4799, 0x6e85076a};
 
-	int[] S1 = {
+	int[] StandardS1 = {
 			0x4b7a70e9, 0xb5b32944, 0xdb75092e, 0xc4192623, 0xad6ea6b0, 0x49a7df7d,
 			0x9cee60b8, 0x8fedb266, 0xecaa8c71, 0x699a17ff, 0x5664526c, 0xc2b19ee1,
 			0x193602a5, 0x75094c29, 0xa0591340, 0xe4183a3e, 0x3f54989a, 0x5b429d65,
@@ -105,7 +105,7 @@ public class Blowfish01
 			0x675fda79, 0xe3674340, 0xc5c43465, 0x713e38d8, 0x3d28f89e, 0xf16dff20,
 			0x153e21e7, 0x8fb03d4a, 0xe6e39f2b, 0xdb83adf7};
 
-	int[] S2 = {
+	int[] StandardS2 = {
 			0xe93d5a68, 0x948140f7, 0xf64c261c, 0x94692934, 0x411520f7, 0x7602d4f7,
 			0xbcf46b2e, 0xd4a20068, 0xd4082471, 0x3320f46a, 0x43b7d4b7, 0x500061af,
 			0x1e39f62e, 0x97244546, 0x14214f74, 0xbf8b8840, 0x4d95fc1d, 0x96b591af,
@@ -150,7 +150,7 @@ public class Blowfish01
 			0xa28514d9, 0x6c51133c, 0x6fd5c7e7, 0x56e14ec4, 0x362abfce, 0xddc6c837,
 			0xd79a3234, 0x92638212, 0x670efa8e, 0x406000e0};
 
-	int[] S3 = {
+	int[] StandardS3 = {
 			0x3a39ce37, 0xd3faf5cf, 0xabc27737, 0x5ac52d1b, 0x5cb0679e, 0x4fa33742,
 			0xd3822740, 0x99bc9bbe, 0xd5118e9d, 0xbf0f7315, 0xd62d1c7e, 0xc700c47b,
 			0xb78c1b6b, 0x21a19045, 0xb26eb1be, 0x6a366eb4, 0x5748ab2f, 0xbc946e79,
@@ -195,7 +195,12 @@ public class Blowfish01
 			0x01c36ae4, 0xd6ebe1f9, 0x90d4f869, 0xa65cdea0, 0x3f09252d, 0xc208e69f,
 			0xb74e6132, 0xce77e25b, 0x578fdfe3, 0x3ac372e6};
 	
-	
+	int[] P = StandardP;
+	int[] S0 = StandardS0;
+	int[] S1 = StandardS1;
+	int[] S2 = StandardS2;
+	int[] S3 = StandardS3;
+	int key_size;
 	/**
 	 * Returns this block cipher's block size in bytes.
 	 *
@@ -214,7 +219,7 @@ public class Blowfish01
 	public int keySize(){
 		//Blowfish has a variable length key ranging from 32-448 bits, 
 		//we have chosen to use a 64 bit key.
-		return 8;
+		return key_size;
 	}
 
 	/**
@@ -225,16 +230,26 @@ public class Blowfish01
 	 */
 	public void setKey
 	   (byte[] key){
-	   int temp;
+	   	   
+	   //Reset to default P,S-Boxes
+	   P  = StandardP;
+	   S0 = StandardS0;
+	   S1 = StandardS1;
+	   S2 = StandardS2;
+	   S3 = StandardS3;
+	   //Force key_size mod 4 == 0, (i.e. is a multiple of 32 bits) 
+	   key_size = key.length / 4 * 4
+	   //Start with all 0s
 	   byte[] generator = { 0,0,0,0,0,0,0,0 };
+
+	   //Xor P with Key bytes
 	   
-	   
-	   for (int i = 0; i < key.length; i+=4)
-	   {	
-	    temp= Packing.packIntBigEndian (key, i);
-	    P[i/4] ^= temp;
+	   for (int i = 0; i < 18 ; i++)
+	   {
+	    P[i] ^= Packing.packIntBigEndian(key, (i*4) % );
 	   }
 	   
+	   //Replace P,S-Box entries by continuously running through encrypt
 	   for(int i = 0; i < 18; i += 2)
 	   {
 	    encrypt(generator);
@@ -299,11 +314,8 @@ public class Blowfish01
 		xR ^= P[16];
 		xL ^= P[17];
 		
-		long longXL = (long) xL;
-		long longXR = (long) xR;
-		longXL <<= 32;
-		long encrypted =  longXL ^ longXR;
-		Packing.unpackLongBigEndian(encrypted, text, 0);
+		Packing.unpackIntBigEndian(xL, text, 0);
+		Packing.unpackIntBigEndian(xR, text, 4);
 	}
 
 	private int F(int x){
